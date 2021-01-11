c6c41fe0d6cb5290851ed367ec433a2ba900d470
Mohammad Shariq <m.shariq22@hotmail.com>
2021-01-12 02:44:48 +0500

    finished crud using lambda and dynamodb

--- handler.js
+++ handler.js
@@ -12,8 +12,27 @@ function response(statusCode, message) {
   };
 }
 
+function sortByDate(a, b) {
+  if (a.createdAt > b.createdAt) {
+    return -1;
+  } else return 1;
+}
+
+// create-post
+
 module.exports.createPost = (event, context, callback) => {
   const reqBody = JSON.parse(event.body);
+
+  if (
+    !reqBody.title ||
+    (reqBody.title.trim() === "" || !reqBody.body) ||
+    reqBody.body.trim() === ""
+  ) {
+    return callback(
+      null,
+      response(400, { error: "Title and Body are required!" })
+    );
+  }
 
   const post = {
     id: uuidv4(),
@@ -33,4 +52,104 @@ module.exports.createPost = (event, context, callback) => {
       callback(null, response(201, post));
     })
     .catch((err) => response(null, response(err.statusCode, err)));
+};
+
+//get all posts
+
+module.exports.getAllPosts = (event, context, callback) => {
+  return db
+    .scan({
+      TableName: postsTable,
+    })
+    .promise()
+    .then((res) => {
+      callback(null, response(200, res.Items.sort(sortByDate)));
+    })
+    .catch((err) => callback(null, response(err.statusCode, err)));
+};
+
+// get number of posts
+
+module.exports.getPosts = (event, context, callback) => {
+  const numberOfPosts = event.pathParameters.number;
+  const params = {
+    TableName: postsTable,
+    Limit: numberOfPosts
+  };
+  return db
+    .scan(params)
+    .promise()
+    .then((res) => {
+      callback(null, response(200, res.Items.sort(sortByDate)));
+    })
+    .catch((err) => callback(null, response(err.statusCode, err)));
+};
+
+//get single post
+
+module.exports.getPost = (event, context, callback) => {
+  const id = event.pathParameters.id;
+
+  const params = {
+    Key: {
+      id: id
+    },
+    TableName: postsTable
+  };
+
+  return db
+    .get(params)
+    .promise()
+    .then((res) => {
+      if (res.Item) callback(null, response(200, res.Item));
+      else callback(null, response(404, { error: 'Post not found' }));
+    })
+    .catch((err) => callback(null, response(err.statusCode, err)));
+};
+// Update a post
+module.exports.updatePost = (event, context, callback) => {
+  const id = event.pathParameters.id;
+  const reqBody = JSON.parse(event.body);
+  const { body, title } = reqBody;
+
+  const params = {
+    Key: {
+      id: id
+    },
+    TableName: postsTable,
+    ConditionExpression: 'attribute_exists(id)',
+    UpdateExpression: 'SET title = :title, body = :body',
+    ExpressionAttributeValues: {
+      ':title': title,
+      ':body': body
+    },
+    ReturnValues: 'ALL_NEW'
+  };
+  console.log('Updating');
+
+  return db
+    .update(params)
+    .promise()
+    .then((res) => {
+      console.log(res);
+      callback(null, response(200, res.Attributes));
+    })
+    .catch((err) => callback(null, response(err.statusCode, err)));
+};
+// Delete a post
+module.exports.deletePost = (event, context, callback) => {
+  const id = event.pathParameters.id;
+  const params = {
+    Key: {
+      id: id
+    },
+    TableName: postsTable
+  };
+  return db
+    .delete(params)
+    .promise()
+    .then(() =>
+      callback(null, response(200, { message: 'Post deleted successfully' }))
+    )
+    .catch((err) => callback(null, response(err.statusCode, err)));
 };
